dc36c2ec86df8ed3a2d256a305a3b2cc94c6e8b3
galoat <florianlacour@hotmail.fr>
2018-12-01 21:52:29 +0100

    add endPoint reservations

--- src/main/kotlin/com/testkafka/demo/DemoApplication.kt
+++ src/main/kotlin/com/testkafka/demo/DemoApplication.kt
@@ -8,21 +8,47 @@ import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.server.*
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
-class DemoApplication
-
-fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+class DemoApplication{
+    @Bean
+    fun routes(@Autowired reservationRepository: ReservationRepository) :RouterFunction<ServerResponse>{
+        return RouterFunctions.route(RequestPredicates.GET("/reservations"), HandlerFunction<ServerResponse> { ServerResponse.ok().body(reservationRepository.findAll()) })
+    }
 }
 
+    fun main(args: Array<String>) {
+        runApplication<DemoApplication>(*args)
+
+    }
+
+
+
+/*
+@RestController
+class ReservationRestController(@Autowired val reservationRepository: ReservationRepository){
+
+    @GetMapping("/reservations")
+     fun reservationFlux():Flux<Reservation>{
+        return this.reservationRepository.findAll();
+
+    }
+
+}*/
+
+
 @Component
-class dataWriter(@Autowired val reservationRepository: ReservationRepository) : ApplicationRunner {
+class DataWriter(@Autowired val reservationRepository: ReservationRepository) : ApplicationRunner {
 
     override fun run(args: ApplicationArguments?) {
         reservationRepository.deleteAll().thenMany(Flux.just("test1", "test2"))
@@ -33,10 +59,8 @@ class dataWriter(@Autowired val reservationRepository: ReservationRepository) : 
     }
 }
 
-    interface ReservationRepository : ReactiveMongoRepository<Reservation, String> {
-
-    }
+    interface ReservationRepository : ReactiveMongoRepository<Reservation, String>
 
 
-    @Document
+@Document
     data class Reservation(@Id var id: String?, val reservationName: String)
