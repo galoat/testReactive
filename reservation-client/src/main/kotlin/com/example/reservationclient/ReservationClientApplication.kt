fc42e96f7dbda60771939debbe3e93e155de06ea
florian lacour <flacour@partner.auchan.com>
2018-12-05 13:07:45 +0100

    add personne sclient

--- reservation-client/src/main/kotlin/com/example/reservationclient/ReservationClientApplication.kt
+++ reservation-client/src/main/kotlin/com/example/reservationclient/ReservationClientApplication.kt
@@ -1,11 +1,50 @@
 package com.example.reservationclient
 
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.context.support.beans
+import org.springframework.web.reactive.function.server.RequestPredicates.path
+import org.springframework.web.reactive.function.server.RouterFunctions.route
 
 @SpringBootApplication
-class ReservationClientApplication
+class ReservationClientApplication {
+/*
+    @Bean
+    fun gateway(rlb: RouteLocatorBuilder) =                beans {
+        bean {
+
+            rlb.routes {
+                route {
+                    path("/proxy").and().host("*.gw.sc")
+                    uri("https://www.google.fr/?gws_rd=ssl")
+                }
+            }
+        }
+    }
+*/
+
+
+}
 
 fun main(args: Array<String>) {
-    runApplication<ReservationClientApplication>(*args)
+    runApplication<ReservationClientApplication>(*args) {
+        addInitializers(
+                beans {
+                    bean {
+                        val rlb = ref<RouteLocatorBuilder>()
+                        rlb.routes {
+                            route {
+                                path("/proxy").and().host("*.gw.sc")
+                                uri("https://www.google.fr/?gws_rd=ssl")
+                            }
+                        }
+                    }
+                })
+    }
 }
